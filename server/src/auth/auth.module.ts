7761262c06cd505d58896cd54386feffe5260c0b
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2026-06-24 17:56:01 +0200

    Removed deprecated, unused (baseUrl)

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
+import type { SignOptions } from "jsonwebtoken";
 import { UsersModule } from "../users/users.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
@@ -17,7 +18,7 @@ import { JwtStrategy } from "./strategies/jwt.strategy";
       useFactory: (configService: ConfigService) => ({
         secret: configService.getOrThrow<string>("JWT_SECRET"),
         signOptions: {
-          expiresIn: configService.get<string>("JWT_EXPIRES_IN", "7d")
+          expiresIn: configService.get<string>("JWT_EXPIRES_IN", "7d") as SignOptions["expiresIn"]
         }
       })
     })
